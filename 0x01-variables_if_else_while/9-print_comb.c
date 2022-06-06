f604f7cb95fd941b77f93ef4608e36777331728d
Elaine <elaingecha@gmail.com>
2022-06-06 13:55:37 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,15 +5,19 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{int m;
+{
+int m;
+
 for (m = 0; m <= 9; m++)
-{putchar((m % 10) + '0');
+{
+putchar((m % 10) + '0');
 if (m == 9)
 continue;
 
 putchar(' , ');
 putchar('  ');
 }
+
 putchar('\n');
 return (0);
 }
