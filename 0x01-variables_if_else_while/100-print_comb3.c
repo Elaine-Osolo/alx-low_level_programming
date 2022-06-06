bdf194946560cf5a6a0aa3b69d3bd542397e7bab
Elaine <elaingecha@gmail.com>
2022-06-06 15:44:05 -0700

    inventing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,19 +7,24 @@
 int main(void)
 {int x, y;
 
-for (x = 0; x < 9; x++)
+for (x = '0'; x <= '9'; x++)
 
 {
-for (y = x + 1; y < 10; y++)
+for (y = '0'; y <= '10'; y++)
+{
+if (x < y)
+{
+putchar(x);
+putchar(y);
 
-{putchar((x % 10) + '0');
-putchar((y % 10) + '0');
-
-if (x == 8 && y == 9)
-continue;
-
-putchar(' , ');
+if (x != '8' || (x == '8' && y != '9'))
+{
+putchar(',');
 putchar(' ');
+}
+}
+}
+}
 
 putchar('\n');
 return (0);
