827662e286727467b961fce9fd63781e784f2ac0
Elaine <elaingecha@gmail.com>
2022-06-06 13:43:40 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,12 @@
 int main(void)
 {int m;
 for (m = 0; m <= 9; m++)
-{putchar(m);
-if (m != 8)
+{putchar((m % 10) + '0');
+if (m == 9)
 continue;
 
 putchar(' , ');
-putchar('');
+putchar('  ');
 }
 putchar('\n');
 return (0);
