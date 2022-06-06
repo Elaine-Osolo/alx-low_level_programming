a795df08b6752f28bd22872211c5ffdfdf26f3fb
Elaine <elaingecha@gmail.com>
2022-06-06 07:52:21 -0700

    lowercase

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,12 +5,10 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{char c = 'z';
+{char c;
 
-while (c >= 'a')
-{putchar(c);
-c--;
-}
+for (c = 'z'; c >= 'a'; c--)
+putchar(c);
 
 putchar('\n');
 return (0);
