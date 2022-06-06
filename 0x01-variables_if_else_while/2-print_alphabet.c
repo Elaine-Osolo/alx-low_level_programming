118a386d7e8bb966328edd5702dfef6ae93ae9dc
Elaine <elaingecha@gmail.com>
2022-06-06 05:39:39 -0700

    Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,19 +5,11 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{int num;
-num = 97;
-while (num <= 122)
-putchar(num);
-num++;
-
-int p;
-p = 97;
-while (p <= 122)
-{putchar(p);
-p++;
+{char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+{putchar(ch);
 }
-
 putchar('\n');
+
 return (0);
 }
