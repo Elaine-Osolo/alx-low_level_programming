d0eca8e861c49529dfaabe9af8974c6fa47f4c04
Elaine <elaingecha@gmail.com>
2022-06-06 16:30:23 -0700

    software

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,25 +1,39 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
- *
+ * Description: prints all possible combination of 2-digit numbers
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int x, y;
+int w, x, y, z;
 {
-for (x = 0; x <= 98; x++)
+for (w = '0'; w <= '99'; x++)
 {
-for (y = x + 1; y <= 99; y++)
+for (x = '0'; x <= '99'; y++)
 {
+for (y = '0'; y <= '99'; y++)
+{
+for (z = '0'; z <= '99'; z++)
+{
+if (((y + z) > (w + x) &&  y >= w || w < y)
+{
+putchar(w);
 putchar(x);
 putchar(y);
-
+putchar(z);
+{
 if (x == 98 && y == 99)
 continue;
 
 putchar(',');
-putchar('');
+putchar(' ');
+}
+}
+}
+}
+}
 }
 }
 putchar('\n');
